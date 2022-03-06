7b31ca59208e43c9061e3351a7313279b09a5c8a
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-06 19:03:52 +0300

    re-arrange the api

--- src/main/java/app/ping/pingring/API/ServerAPI.java
+++ src/main/java/app/ping/pingring/API/ServerAPI.java
@@ -1,12 +1,20 @@
 package app.ping.pingring.API;
 
+import app.ping.pingring.Entity.HttpResponse;
 import app.ping.pingring.Utility.Implementation.ServerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.OK;
+
 @RestController
-@RequestMapping("/servers")
+@RequestMapping("/server")
 public class ServerAPI {
 
     private ServerServiceImpl serverService;
@@ -14,5 +22,18 @@ public class ServerAPI {
     @Autowired
     public ServerAPI(ServerServiceImpl serverService) {
         this.serverService = serverService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<HttpResponse> getServers() {
+        return ResponseEntity.ok(
+          HttpResponse.builder()
+                  .timeStamp(now())
+                  .data(Map.of("servers", serverService.all(30)))
+                  .message("server found")
+                  .status(OK)
+                  .statusCode(OK.value())
+                  .build()
+        );
     }
 }
