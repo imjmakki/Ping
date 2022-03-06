0b565bbefb919f1bc1ab5b15102f0386b0ab6f55
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-06 14:15:12 +0300

    Starting ping project

---
+++ src/main/java/app/ping/pingring/PingRingApplication.java
@@ -0,0 +1,13 @@
+package app.ping.pingring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingRingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PingRingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/ping/pingring/PingRingApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ping.pingring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingRingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
