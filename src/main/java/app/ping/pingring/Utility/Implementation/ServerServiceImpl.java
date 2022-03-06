db0a9cd2dd8fbc123918c53f68fac36b481facc4
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-06 16:43:36 +0300

    updating the implementations

--- src/main/java/app/ping/pingring/Utility/Implementation/ServerServiceImpl.java
+++ src/main/java/app/ping/pingring/Utility/Implementation/ServerServiceImpl.java
@@ -1,7 +1,9 @@
 package app.ping.pingring.Utility.Implementation;
 
+import app.ping.pingring.DAO.ServerDAO;
 import app.ping.pingring.Entity.Server;
 import app.ping.pingring.Service.ServerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,6 +12,14 @@ import java.util.Collection;
 @Service
 @Transactional
 public class ServerServiceImpl implements ServerService {
+
+    private ServerDAO serverDAO;
+
+    @Autowired
+    public ServerServiceImpl(ServerDAO serverDAO) {
+        this.serverDAO = serverDAO;
+    }
+
     @Override
     public Server addServer(Server server) {
         return null;
