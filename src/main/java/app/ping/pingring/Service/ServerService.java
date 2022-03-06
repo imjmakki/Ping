e3c78687d487c722096cdb61956fe67ed2ad0348
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-06 15:54:43 +0300

    starting the configurations inside the services

--- src/main/java/app/ping/pingring/Service/ServerService.java
+++ src/main/java/app/ping/pingring/Service/ServerService.java
@@ -1,7 +1,13 @@
 package app.ping.pingring.Service;
 
+import app.ping.pingring.Entity.Server;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public interface ServerService {
+
+    Server add(Server server);
+    Collection<Server> all(Integer limit);
 }
