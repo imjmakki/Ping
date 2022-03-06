d746331b3d444837b2242cb3104322e27dee56f3
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-06 20:06:46 +0300

    updating the data

--- src/main/java/app/ping/pingring/PingRingApplication.java
+++ src/main/java/app/ping/pingring/PingRingApplication.java
@@ -19,13 +19,13 @@ public class PingRingApplication {
     @Bean
     CommandLineRunner run(ServerDAO serverDAO) {
         return args -> {
-          serverDAO.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "1 TB", "Marketing",
+          serverDAO.save(new Server(null, "192.168.1.160", "Marketing", "1 TB", "Ubuntu Linux",
                   "http://localhost:8080/server/image/server2.png", SERVER_UP));
-          serverDAO.save(new Server(null, "192.168.1.56", "Redhat Linux", "2 TB", "HR",
+          serverDAO.save(new Server(null, "192.168.1.56", "HR", "2 TB", "Redhat Linux",
                     "http://localhost:8080/server/image/server5.png", SERVER_DOWN));
-          serverDAO.save(new Server(null, "192.168.1.21", "Fedora Linux", "1.5 TB", "Accounting",
+          serverDAO.save(new Server(null, "192.168.1.21", "Accounting", "1.5 TB", "Fedora Linux",
                     "http://localhost:8080/server/image/server6.png", SERVER_UP));
-          serverDAO.save(new Server(null, "192.168.1.16", "Ubuntu Linux", "10 TB", "Software",
+          serverDAO.save(new Server(null, "192.168.1.16", "Software", "10 TB", "Ubuntu Linux",
                     "http://localhost:8080/server/image/server8.png", SERVER_DOWN));
         };
     }
