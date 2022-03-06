418597b50f42016d19de63aaa78dee97f8d08705
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-06 19:51:33 +0300

    adding some demos

--- src/main/java/app/ping/pingring/PingRingApplication.java
+++ src/main/java/app/ping/pingring/PingRingApplication.java
@@ -1,5 +1,9 @@
 package app.ping.pingring;
 
+import app.ping.pingring.DAO.ServerDAO;
+import app.ping.pingring.Entity.Server;
+import app.ping.pingring.Utility.enums.Status;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +14,10 @@ public class PingRingApplication {
         SpringApplication.run(PingRingApplication.class, args);
     }
 
+    CommandLineRunner run(ServerDAO serverDAO) {
+        return args -> {
+          serverDAO.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "1 TB", "PC",
+                  "http://localhost:8080/server/image/server2.png", Status.SERVER_UP));
+        };
+    }
 }
