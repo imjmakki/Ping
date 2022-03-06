644ad7f512e9b030ae1078790d36acdb05dbafdc
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-06 17:14:14 +0300

    adding implementation for getting server with id

--- src/main/java/app/ping/pingring/Utility/Implementation/ServerServiceImpl.java
+++ src/main/java/app/ping/pingring/Utility/Implementation/ServerServiceImpl.java
@@ -52,7 +52,8 @@ public class ServerServiceImpl implements ServerService {
 
     @Override
     public Server getServer(Long id) {
-        return null;
+        log.info("Grabbing server in the system with specific id: {}", id);
+        return serverDAO.findById(id).get();
     }
 
     @Override
