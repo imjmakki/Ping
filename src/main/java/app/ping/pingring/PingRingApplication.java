8a89626a788d431e87235a063b44659e2c2a0cdc
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-06 19:55:15 +0300

    adding some demos

--- src/main/java/app/ping/pingring/PingRingApplication.java
+++ src/main/java/app/ping/pingring/PingRingApplication.java
@@ -2,10 +2,12 @@ package app.ping.pingring;
 
 import app.ping.pingring.DAO.ServerDAO;
 import app.ping.pingring.Entity.Server;
-import app.ping.pingring.Utility.enums.Status;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static app.ping.pingring.Utility.enums.Status.*;
 
 @SpringBootApplication
 public class PingRingApplication {
@@ -14,10 +16,17 @@ public class PingRingApplication {
         SpringApplication.run(PingRingApplication.class, args);
     }
 
+    @Bean
     CommandLineRunner run(ServerDAO serverDAO) {
         return args -> {
-          serverDAO.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "1 TB", "PC",
-                  "http://localhost:8080/server/image/server2.png", Status.SERVER_UP));
+          serverDAO.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "1 TB", "Marketing",
+                  "http://localhost:8080/server/image/server2.png", SERVER_UP));
+          serverDAO.save(new Server(null, "192.168.1.56", "Redhat Linux", "2 TB", "HR",
+                    "http://localhost:8080/server/image/server5.png", SERVER_DOWN));
+          serverDAO.save(new Server(null, "192.168.1.21", "Fedora Linux", "1.5 TB", "Accounting",
+                    "http://localhost:8080/server/image/server6.png", SERVER_UP));
+          serverDAO.save(new Server(null, "192.168.1.16", "Ubuntu Linux", "10 TB", "Software",
+                    "http://localhost:8080/server/image/server8.png", SERVER_DOWN));
         };
     }
 }
