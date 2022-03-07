27efaffa2f8b565770090355bdfb9c93b43c3dcd
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-07 12:19:41 +0300

    adding the error state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {ServerService} from "./service/server.service";
-import {Observable, startWith} from "rxjs";
+import {Observable, of, startWith} from "rxjs";
 import {AppState} from "./interface/app-state";
 import {CustomResponse} from "./interface/custom-response";
 import {DataState} from "./enum/data-state.enum";
+import {catchError, map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -20,7 +21,10 @@ export class AppComponent implements OnInit {
         map(response => {
           return { dataState: DataState.LOADED_STATE, appData: response }
         }),
-        startWith({ dataState: DataState.LOADING_STATE })
+        startWith({ dataState: DataState.LOADING_STATE }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE })
+        })
       );
   }
 
