1139566fa75913ae1bcbf0497ed4180a583f7fa2
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-07 12:07:14 +0300

    start uploading the angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ServerService} from "./service/server.service";
+import {Observable} from "rxjs";
+import {AppState} from "./interface/app-state";
+import {CustomResponse} from "./interface/custom-response";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Pingular';
+export class AppComponent implements OnInit {
+  appState$: Observable<AppState<CustomResponse>>;
+  constructor(private serverService: ServerService) {}
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$
+      .pipe(
+        map(response => {
+          return {}
+        })
+      );
+  }
+
 }
